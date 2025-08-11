e94c9433e5825dc83ad4b8b352885e883f0fb970
diydriller <dhrhd080@naver.com>
2025-08-12 00:49:59 +0900

    feat: multimodule - discovery service module

---
+++ discovery-service/build.gradle.kts
@@ -0,0 +1,26 @@
+tasks.getByName("bootJar") {
+    enabled = true
+}
+
+tasks.getByName("jar") {
+    enabled = false
+}
+
+ext {
+    set("springCloudVersion", "2023.0.3")
+}
+
+dependencies {
+    implementation ("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom ("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
+    imageName.set("diydriller/${rootProject.name}-${project.name}")
+}
+
