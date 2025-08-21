7533247bcba30d0cb476275d89740d27e6e2e8a6
diydriller <dhrhd080@naver.com>
2025-08-21 18:47:27 +0900

    feat: user service - grpc server 설정 & profile 조회 로직

---
+++ user-service/src/main/kotlin/com/xircle/userservice/application/grpc/GetMemberProfileServiceImpl.kt
@@ -0,0 +1,52 @@
+package com.xircle.userservice.application.grpc
+
+import com.xircle.userservice.application.grpc.proto.*
+import com.xircle.userservice.domain.integration.reader.MemberReader
+import io.grpc.stub.StreamObserver
+import net.devh.boot.grpc.server.service.GrpcService
+
+@GrpcService
+class GetMemberProfileServiceImpl(
+    private val memberReader: MemberReader
+) : GetMemberProfileServiceGrpc.GetMemberProfileServiceImplBase() {
+    override fun getMemberProfileListCall(
+        request: GetMemberProfileListRequest,
+        responseObserver: StreamObserver<GetMemberProfileListResponse>
+    ) {
+        val memberInfoList = memberReader.findMemberByIdListIn(request.userIdList).map { member ->
+            GetMemberProfileInfo.newBuilder()
+                .setProfileImage(member.profileImage)
+                .setId(member.id!!)
+                .setNickname(member.nickname)
+                .setEmail(member.email)
+                .build()
+        }
+
+        val response = GetMemberProfileListResponse.newBuilder()
+            .addAllInfo(memberInfoList)
+            .build()
+
+        responseObserver.onNext(response)
+        responseObserver.onCompleted()
+    }
+
+    override fun getMemberProfileCall(
+        request: GetMemberProfileRequest,
+        responseObserver: StreamObserver<GetMemberProfileResponse>
+    ) {
+        val member = memberReader.findMemberById(request.userId)
+        val memberInfo = GetMemberProfileInfo.newBuilder()
+            .setProfileImage(member.profileImage)
+            .setId(member.id!!)
+            .setNickname(member.nickname)
+            .setEmail(member.email)
+            .build()
+
+        val response = GetMemberProfileResponse.newBuilder()
+            .setInfo(memberInfo)
+            .build()
+
+        responseObserver.onNext(response)
+        responseObserver.onCompleted()
+    }
+}
