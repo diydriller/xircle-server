0fe41532418dd1e7140b9d1065f910d10fb7e594
diydriller <dhrhd080@naver.com>
2025-08-21 18:55:44 +0900

    feat: gateway service - grpc client 설정 & profile 조회 rpc 호출

--- gateway-service/src/main/kotlin/com/xircle/gatewayservice/WebConfig.kt
+++ gateway-service/src/main/kotlin/com/xircle/gatewayservice/WebConfig.kt
@@ -1,8 +1,10 @@
 package com.xircle.gatewayservice
 
+import net.devh.boot.grpc.client.inject.GrpcClientBeanPostProcessor
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
 
 @Configuration
 class WebConfig {
