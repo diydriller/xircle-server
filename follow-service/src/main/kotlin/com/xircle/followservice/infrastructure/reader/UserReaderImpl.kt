2bb03828f3e7167283d844940f47f68f9c00abe1
diydriller <dhrhd080@naver.com>
2025-08-21 18:53:43 +0900

    feat: follow service - grpc client 설정 & profile 조회 rpc 호출

--- follow-service/src/main/kotlin/com/xircle/followservice/infrastructure/reader/UserReaderImpl.kt
+++ follow-service/src/main/kotlin/com/xircle/followservice/infrastructure/reader/UserReaderImpl.kt
@@ -2,14 +2,29 @@ package com.xircle.followservice.infrastructure.reader
 
 import com.xircle.common.dto.MemberInfo
 import com.xircle.followservice.domain.integration.reader.UserReader
-import com.xircle.followservice.infrastructure.api.client.UserServiceClient
+import com.xircle.userservice.application.grpc.proto.GetMemberProfileListRequest
+import com.xircle.userservice.application.grpc.proto.GetMemberProfileServiceGrpc
+import net.devh.boot.grpc.client.inject.GrpcClient
 import org.springframework.stereotype.Component
 
 @Component
 class UserReaderImpl(
-    private val userServiceClient: UserServiceClient
+    @GrpcClient("local-grpc-server")
+    private val getMemberProfileServiceBlockingStub: GetMemberProfileServiceGrpc.GetMemberProfileServiceBlockingStub
 ) : UserReader {
     override fun getMemberInfoList(memberIdList: List<Long>): List<MemberInfo> {
-        return userServiceClient.getMemberInfoList(memberIdList)
+        val request = GetMemberProfileListRequest.newBuilder()
+            .addAllUserId(memberIdList)
+            .build()
+
+        val response = getMemberProfileServiceBlockingStub.getMemberProfileListCall(request).infoList.map { info ->
+            MemberInfo(
+                id = info.id,
+                email = info.email,
+                profileImage = info.profileImage,
+                nickname = info.nickname
+            )
+        }
+        return response
     }
 }
